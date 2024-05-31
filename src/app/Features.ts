af26baa33746d79801714e5ec60b5b0bf8044327
Mks1311 <manish31311@gmail.com>
2024-06-01 00:19:27 +0530

    Station Data

--- src/app/Features.ts
+++ src/app/Features.ts
@@ -10,6 +10,15 @@ type IUser = {
     UserMobile: number,
 };
 
+type StationData = {
+    StationName: string,
+    StationAddress: string,
+    StaionCode: string,
+    NumberChargers: number,
+    AvailableChargers: number,
+    NextAvailableCharger: number,
+}
+
 const UserId = localStorage.getItem("UserId")
 const UserName = localStorage.getItem("UserName")
 const UserMail = localStorage.getItem("UserMail")
@@ -17,18 +26,34 @@ const UserImageUrl = localStorage.getItem("UserImageUrl")
 const UserRole = localStorage.getItem("UserRole")
 const UserMobile = localStorage.getItem("UserMobile")
 
+const StationName = localStorage.getItem("StationName")
+const StationAddress = localStorage.getItem("StationAddress")
+const StaionCode = localStorage.getItem("StaionCode")
+const NumberChargers = localStorage.getItem("NumberChargers")
+const AvailableChargers = localStorage.getItem("AvailableChargers")
+const NextAvailableCharger = localStorage.getItem("NextAvailableCharger")
+
 export interface UserState {
     User: IUser
+    Station: StationData
 }
 
 const initialState: UserState = {
     User: {
-        UserId: UserId!== null ? UserId : "",
+        UserId: UserId !== null ? UserId : "",
         UserName: UserName !== null ? UserName : "",
         UserMail: UserMail !== null ? UserMail : "",
         UserImageUrl: UserImageUrl !== null ? UserImageUrl : "",
         UserRole: UserRole !== null ? UserRole : "",
         UserMobile: UserMobile !== null ? Number(UserMobile) : 0,
+    },
+    Station: {
+        StationName: StationName !== null ? StationName : "",
+        StationAddress: StationAddress !== null ? StationAddress : "",
+        StaionCode: StaionCode !== null ? StaionCode : "",
+        NumberChargers: NumberChargers !== null ? Number(NumberChargers) : 0,
+        AvailableChargers: AvailableChargers !== null ? Number(AvailableChargers) : 0,
+        NextAvailableCharger: NextAvailableCharger != null ? Number(NextAvailableCharger) : 0
     }
 }
 
@@ -44,6 +69,16 @@ export const UserSlice = createSlice({
             localStorage.setItem("UserRole", (state.User.UserRole))
             localStorage.setItem("UserMobile", String(state.User.UserMobile))
         },
+
+        setStation(state, action: PayloadAction<StationData>) {
+            state.Station = action.payload;
+            localStorage.setItem("StationName", (state.Station.StationName))
+            localStorage.setItem("StationAddress", (state.Station.StationAddress))
+            localStorage.setItem("StaionCode", (state.Station.StaionCode))
+            localStorage.setItem("NumberChargers", String(state.Station.NumberChargers))
+            localStorage.setItem("AvailableChargers", String(state.Station.AvailableChargers))
+            localStorage.setItem("NextAvailableCharger", String(state.Station.NextAvailableCharger))
+        }
     },
 })
 
